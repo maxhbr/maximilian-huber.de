198757787ebda149ad9fd59402b3ad4258954817
maximilianhuber <mail@maximilian-huber.de>
2015-04-23 09:36:12 +0200

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -5,10 +5,21 @@ let pkgs = (import <nixpkgs> {});
         in {
             thisPackage = haskellPackages.callPackage (import ./default.nix) {};
     };});
+    hsEnv = pkgs.haskellPackages.ghcWithPackages (hsPkgs : ([
+        hsPkgs.hlint
+        hsPkgs.pointfree
+        hsPkgs.hdevtools
+        hsPkgs.hasktags
+    ]));
 in pkgs.lib.overrideDerivation haskellPackages.thisPackage (old: {
     buildInputs = old.buildInputs ++ [
         haskellPackages.cabalInstall
         pkgs.lftp
         pkgs.openssl
         pkgs.imagemagick
-    ];})
+        hsEnv
+    ];
+    extraCmds = ''
+        $(grep export ${hsEnv.outPath}/bin/ghc)
+    '';
+    })
